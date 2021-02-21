d4ebd4dc49ed711d2baab28e144ff9dafd40136a
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-22 01:26:29 +0600

    adding script and css

---
+++ Template/script.js
@@ -0,0 +1,27 @@
+const url = "https://jsonplaceholder.typicode.com/users"
+
+
+
+fetch(url)
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => showUsersName(data));
+
+
+const showUsersName = (userInfo) => {
+    // console.log(userInfo);
+
+    let userHTML = ''
+    userInfo.forEach(element => {
+        userHTML = userHTML +
+            `<div class="user col-lg-3 col-md-6 col-sm-12">
+                <h3>${element.id}. ${element.name}</h3>
+                <p>City: ${element.address.city}, Street: ${element.address.street}</p>
+                <p>Phone: ${element.address.phone}, Email: ${element.email}</p>
+            </div>`
+    });
+    const showList = document.getElementById("user-list")
+    // console.log(ul);
+    showList.innerHTML = userHTML;
+
+}
